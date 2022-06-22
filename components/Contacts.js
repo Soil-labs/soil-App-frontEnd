386c7b4122c81c2445828792a89888bf6ba6fde5
sbelka-1703 <sbelka93@gmail.com>
2022-06-22 13:48:42 -0700

    fixed apostrophe bug that prevented deployment

--- components/Contacts.js
+++ components/Contacts.js
@@ -205,7 +205,7 @@ export default function Contacts() {
                 type="submit"
                 className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Let's talk
+                Let&apos;s talk
               </button>
             </div>
           </form>
