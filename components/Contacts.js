92a89b67a7fa76b2e0ff2e8a813d5b319b2a6df1
sbelka-1703 <sbelka93@gmail.com>
2022-06-22 13:56:57 -0700

    fixed a bug that was preventing vercel deployment due to case difference in navbar import statement

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,13 +1,12 @@
-
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Contacts() {
-  const [agreed, setAgreed] = useState(false)
+  const [agreed, setAgreed] = useState(false);
 
   return (
     <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
@@ -29,10 +28,21 @@ export default function Contacts() {
               height={20}
               patternUnits="userSpaceOnUse"
             >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
             </pattern>
           </defs>
-          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
         </svg>
         <svg
           className="absolute right-full bottom-0 transform -translate-x-1/2"
@@ -51,22 +61,42 @@ export default function Contacts() {
               height={20}
               patternUnits="userSpaceOnUse"
             >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
             </pattern>
           </defs>
-          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
         </svg>
         <div className="text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Contact sales
+          </h2>
           <p className="mt-4 text-lg leading-6 text-gray-500">
-            Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
-            arcu.
+            Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat
+            massa dictumst amet. Sapien tortor lacus arcu.
           </p>
         </div>
         <div className="mt-12">
-          <form action="#" method="POST" className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+          <form
+            action="#"
+            method="POST"
+            className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+          >
             <div>
-              <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium text-gray-700"
+              >
                 First name
               </label>
               <div className="mt-1">
@@ -80,7 +110,10 @@ export default function Contacts() {
               </div>
             </div>
             <div>
-              <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Last name
               </label>
               <div className="mt-1">
@@ -94,7 +127,10 @@ export default function Contacts() {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="company" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="company"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Company
               </label>
               <div className="mt-1">
@@ -108,7 +144,10 @@ export default function Contacts() {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Email
               </label>
               <div className="mt-1">
@@ -122,7 +161,10 @@ export default function Contacts() {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="phone-number" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Phone Number
               </label>
               <div className="mt-1 relative rounded-md shadow-sm">
@@ -151,7 +193,10 @@ export default function Contacts() {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Message
               </label>
               <div className="mt-1">
@@ -160,7 +205,7 @@ export default function Contacts() {
                   name="message"
                   rows={4}
                   className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
-                  defaultValue={''}
+                  defaultValue={""}
                 />
               </div>
             </div>
@@ -171,27 +216,27 @@ export default function Contacts() {
                     checked={agreed}
                     onChange={setAgreed}
                     className={classNames(
-                      agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                      'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      agreed ? "bg-indigo-600" : "bg-gray-200",
+                      "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     )}
                   >
                     <span className="sr-only">Agree to policies</span>
                     <span
                       aria-hidden="true"
                       className={classNames(
-                        agreed ? 'translate-x-5' : 'translate-x-0',
-                        'inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                        agreed ? "translate-x-5" : "translate-x-0",
+                        "inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
                       )}
                     />
                   </Switch>
                 </div>
                 <div className="ml-3">
                   <p className="text-base text-gray-500">
-                    By selecting this, you agree to the{' '}
+                    By selecting this, you agree to the{" "}
                     <a href="#" className="font-medium text-gray-700 underline">
                       Privacy Policy
-                    </a>{' '}
-                    and{' '}
+                    </a>{" "}
+                    and{" "}
                     <a href="#" className="font-medium text-gray-700 underline">
                       Cookie Policy
                     </a>
@@ -212,5 +257,5 @@ export default function Contacts() {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import CTA from "../components/CTA";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Stats from "../components/Stats";
 import Testimonials from "../components/Testimonials";
 
