1308aa6aa02fdf39017a6f87c191493c019ab80b
sbelka-1703 <sbelka93@gmail.com>
2022-06-29 18:24:40 -0700

    created the form with two questions. The reply updates correctly but the input field does not clear

---
+++ pages/form/index.js
@@ -0,0 +1,57 @@
+import FormComponent from "../../components/FormComponent";
+import { useState } from "react";
+
+function Form() {
+  const [phase, setPhase] = useState(0);
+  const [replyValue, setReplyValue] = useState("");
+  const [questions, setQuestions] = useState([
+    {
+      title: "What’s the tilte of the new project?",
+      description: "description 1",
+      reply: "",
+    },
+    {
+      title: "Description of the new project?",
+      description:
+        "Key info on what will make the filling curcial: for ex - great titles are  short & descriptive bla bla",
+      reply: "",
+    },
+  ]);
+
+  const changePhase = () => {
+    if (questions.length - 2 >= phase) {
+      setPhase((phase += 1));
+    } else {
+      return;
+    }
+  };
+
+
+  const handleChange = (e) => {
+    setReplyValue(e.target.value);
+  };
+
+  const updateReply = () => {
+    let newArr = [...questions];
+    console.log(phase)
+    newArr[phase].reply = replyValue;
+    setQuestions(newArr);
+    console.log(questions);
+    setReplyValue(" ")
+  };
+
+  return (
+    <>
+      <FormComponent
+        handleChange={handleChange}
+        updateReply={updateReply}
+        setReplyValue={setReplyValue}
+        changePhase={changePhase}
+        questions={questions[phase]}
+      />
+      {/* <FormComponent changePhase={changePhase} questions={questions[phase]} /> */}
+    </>
+  );
+}
+
+export default Form;
