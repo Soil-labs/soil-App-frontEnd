bbb665661f7efa0f6325098af468e3c050736c9a
sbelka-1703 <sbelka93@gmail.com>
2022-07-03 14:02:32 -0700

    asyncThunk created

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,7 +1,7 @@
 import FormComponent from "../../components/FormComponent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getData, addReply, createNewProject } from "../../redux/slices/projectSlice";
+import { createNewProject, findProject } from "../../redux/slices/projectSlice";
 
 function Form() {
   const [phase, setPhase] = useState(0);
@@ -20,18 +20,16 @@ function Form() {
   ]);
 
   const changePhase = (phaseNow) => {
-
-    console.log("questions.length > phaseNow = " , questions.length, phaseNow)
-    if (questions.length  - 1> phaseNow) {
+    console.log("questions.length > phaseNow = ", questions.length, phaseNow);
+    if (questions.length - 1 > phaseNow) {
       setPhase((phaseNow += 1));
     } else {
-      submitReply()
+      // submitReply();
     }
   };
 
   const project = useSelector((state) => state.project);
   const dispatch = useDispatch();
-  
 
   const handleChange = (e, phaseNow) => {
     let newArr = [...questions];
@@ -39,21 +37,42 @@ function Form() {
     newArr[phaseNow].reply = e.target.value;
 
     setQuestions(newArr);
-    console.log(questions)
+    console.log(questions);
   };
 
   const submitReply = () => {
-    console.log("we are at the end of the FORM!! = ",questions[0].reply,questions[1].reply )
-
+    console.log(
+      "we are at the end of the FORM!! = ",
+      questions[0].reply,
+      questions[1].reply
+    );
     const feild = {
       title: questions[0].reply,
       description: questions[1].reply,
-    }
+    };
     dispatch(createNewProject(feild));
-    // dispatch(createNewProject(questions[0].reply,questions[1].reply));
-    // dispatch(addReply(questions));
-  }
+  };
 
+  // const lookForProject = () => {
+  //   const field = {
+  //     _id: "62a47e56020ea30004c1f231"
+  //   }
+  //   dispatch()
+  // }
+
+  useEffect(() => {
+    const lookForProject = () => {
+      const field = {
+        _id: "62c0dac5a38139000437e607"
+      };
+
+      console.log("this is the _id in the form ============>>>>>>>>",field._id)
+      dispatch(findProject(field))
+    }
+    lookForProject()
+  }, [phase])
+  
+ 
   return (
     <>
       <FormComponent
@@ -61,7 +80,6 @@ function Form() {
         changePhase={changePhase}
         questions={questions[phase]}
         phase={phase}
-        submitReply ={submitReply}
       />
       {/* <FormComponent changePhase={changePhase} questions={questions[phase]} /> */}
     </>
--- redux/slices/projectSlice.js
+++ redux/slices/projectSlice.js
@@ -4,14 +4,16 @@ import apiClient from "../../pages/api/axios";
 const initialState = {
   title: "",
   description: "",
+  _id: "",
 };
 
-export const createNewProject = createAsyncThunk("createNewProject", async (feild) => {
-
-  console.log("title,description = " , feild.title,feild.description)
-  const response = await apiClient({
-    data: {
-      query: `mutation{
+export const createNewProject = createAsyncThunk(
+  "createNewProject",
+  async (feild) => {
+    console.log("title,description = ", feild.title, feild.description);
+    const response = await apiClient({
+      data: {
+        query: `mutation{
       updateProject(fields:{
         tagName: "${feild.title}"
         title: "${feild.title}"
@@ -22,28 +24,53 @@ export const createNewProject = createAsyncThunk("createNewProject", async (feil
         description
       }
     }`,
+      },
+    });
+
+    console.log("response.date.data = ", response.data.data.updateProject);
+
+    return response.data.data.updateProject;
+  }
+);
+
+export const findProject = createAsyncThunk("findProject", async (field) => {
+  const response = await apiClient({
+    data: {
+      query: `query{
+        findProject(fields:{
+          _id: "${field._id}"
+        }){
+          _id
+          title
+          description
+        }
+      }`,
     },
   });
+  console.log("response.date.data = ", response.data.data.findProject);
 
-  console.log("response.date.data = " , response.data.data.updateProject)
-
-  return response.data.data.updateProject;
+  return response.data.data.findProject;
 });
 
 export const projectSlice = createSlice({
   name: "project",
   initialState,
-  reducers: {
+  reducers: {},
+  extraReducers: {
+    [createNewProject.fulfilled]: (state, { payload }) => {
+      // console.log("reducerWorks - state= ", state);
+      // console.log("reducerWorks2 - payload = ", payload);
+
+      state.title = payload.title;
+      state.description = payload.description;
     },
-    extraReducers: {
-      [createNewProject.fulfilled]: (state,{payload}) => {
-        console.log("reducerWorks - state= " , state)
-        console.log("reducerWorks2 - payload = " , payload)
-
-        state.title = payload.title;
-        state.description = payload.description;
-
-      },
+    [findProject.fulfilled]: (state, { payload }) => {
+      console.log("reducerWorks - state= ", state);
+      console.log("reducerWorks2 - payload = ", payload);
+      state._id = payload._id;
+      state.title = payload.title;
+      state.description = payload.description;
+    },
   },
 });
 
