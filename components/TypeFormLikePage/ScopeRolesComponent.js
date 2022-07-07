42b1a2c9aaf43bb0e9d7e1ce101fca43bdf6b155
sbelka-1703 <sbelka93@gmail.com>
2022-07-07 15:08:59 -0700

    saving work

--- components/TypeFormLikePage/ScopeRolesComponent.js
+++ components/TypeFormLikePage/ScopeRolesComponent.js
@@ -16,14 +16,12 @@ function ScopeRolesComponent(props) {
   ]);
 
   const handleChange = e => {
-    const {role,value} = e.target;
+    const { role, value } = e.target;
     setRoleList({
       ...roleList,
-      [role]: value
-
-    })
-  }
-  
+      [role]: value,
+    });
+  };
 
   const ScopeRolesForm = (props) => {
     return (
@@ -35,29 +33,28 @@ function ScopeRolesComponent(props) {
           </div>
           <div>
             <div className=" w-96 h-[650px] flex flex-col justify-center items-center space-y-6 ">
-            <div className="mt-1 relative rounded-2xl">
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <UserIcon
-            className="h-[45px] w-[47.21px] text-gray-400"
-            aria-hidden="true"
-          />
-        </div>
-        <input
-            onChange={handleChange
-            }
-          type="text"
-          className="focus:ring-indigo-500 focus:border-indigo-500 block w-[303px] h-[60px]
+              <div className="relative mt-1 rounded-2xl">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <UserIcon
+                    className="h-[45px] w-[47.21px] text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-[303px] h-[60px]
                        pl-16 sm:text-sm border-gray-300  rounded-3xl"
-          placeholder="TYPE THE DESIRED ROLE"
-          value={roleList.roleType}
-          name="role"
-        />
-      </div>
+                  placeholder="TYPE THE DESIRED ROLE"
+                  value={roleList.roleType}
+                  name="roleType"
+                />
+              </div>
               <button
                 type="button"
                 className="inline-flex items-center p-1.5 border border-transparent rounded-full shadow-sm text-black bg-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                <PlusSmIconSolid className="h-5 w-5" aria-hidden="true" />
+                <PlusSmIconSolid className="w-5 h-5" aria-hidden="true" />
               </button>
             </div>
           </div>
@@ -67,11 +64,11 @@ function ScopeRolesComponent(props) {
               props.changePhase(props.phase);
             }}
           >
-            <ChevronDoubleDownIcon className="h-10 w-10 text-black mt-10 font-light stroke-1" />
+            <ChevronDoubleDownIcon className="w-10 h-10 mt-10 font-light text-black stroke-1" />
           </button>
         </div>
 
-        <pre>{JSON.stringify(roleList,null,2)}</pre>
+        <pre>{JSON.stringify(roleList, null, 2)}</pre>
       </div>
     );
   };
