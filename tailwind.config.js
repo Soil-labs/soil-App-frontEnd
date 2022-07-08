76bcc9bb8428f92002565387a1327b0473d1529d
heavensbee001 <heavensbee001@gmail.com>
2022-07-08 11:45:32 +0200

    Add some colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'onion': "url('/public/vercel.svg')",
+        onion: "url('/public/vercel.svg')",
       },
       colors: {
         soilGreen: {
           50: "#ACCEA0",
+          70: "#6BAF22",
           100: "#268A02",
         },
+        bgGrey: "#F1F6F7",
       },
     },
   },
