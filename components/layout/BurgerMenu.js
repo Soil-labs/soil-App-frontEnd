cacb5b6e0aba177384c6a4407c81c0a7b4b631d2
heavensbee001 <heavensbee001@gmail.com>
2022-07-08 11:46:06 +0200

    Add Burger MEnu Component

---
+++ components/layout/BurgerMenu.js
@@ -0,0 +1,65 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+
+const resources = [
+  {
+    name: "🚀 Launch a Project",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "🤝 Join a Project",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "👤 Edit Profile",
+    description: "",
+    href: "#",
+  },
+];
+
+export default function BurgerMenu() {
+  return (
+    <Popover className="flex justify-end">
+      <div className="max-w-7xl">
+        <Popover.Button className="border border-gray-200 rounded-full p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none">
+          <span className="sr-only">Open menu</span>
+          <MenuIcon className="h-5 w-5" aria-hidden="true" />
+        </Popover.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute w-52 top-16 right-0 transition transform origin-top-right drop-shadow-[0px_4px_30px_rgba(0,0,0,0.2)]"
+        >
+          <div className="absolute w-4 h-4 right-8 -top-2 bg-white rotate-45"></div>
+          <div className="rounded-lg bg-white">
+            <div className="px-3 py-1 divide-y">
+              {resources.map((item) => (
+                <div className="px-3 py-2" key={item.name}>
+                  <Link href={item.href}>
+                    <a className="text-gray-900 hover:text-gray-600 cursor-pointer">
+                      {item.name}
+                    </a>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  );
+}
