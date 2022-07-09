f5d951c36dd0e84383b48761f10cdf11c15d26ec
sbelka-1703 <sbelka93@gmail.com>
2022-07-09 13:54:51 -0700

    f

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/counterSlice";
+// import counterReducer from "./slices/counterSlice";
 import memberReducer from "./slices/memberSlice";
 import projectReducer from "./slices/projectSlice";
 import skillReducer from "./slices/skillSlice";
 
 
 export const store = configureStore({
-  reducer: { counter: counterReducer, member: memberReducer, project: projectReducer, skill: skillReducer },
+  reducer: { member: memberReducer, project: projectReducer, skill: skillReducer },
 });
