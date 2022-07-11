2ac1a83b31b84dac60d8fec8daf98ce45c3ed4e8
Harpalsinh <jadejaharpal14@gmail.com>
2022-07-11 16:10:16 +0530

    inspectUsers slice

--- redux/slices/inspectUsers.js
+++ redux/slices/inspectUsers.js
@@ -20,8 +20,7 @@ async function fetchMembers(tagName) {
                   discordName
                   discordAvatar
                   discriminator
-                }
-                  
+                }     
               }
             }`,
         },
