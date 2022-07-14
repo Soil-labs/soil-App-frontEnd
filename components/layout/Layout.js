99332de299e78197f1bb6f7a6b15edc6d7359e68
subject026 <subject026@protonmail.com>
2022-07-14 12:59:52 +0100

    removed custom routing logic

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,35 +1,12 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Header from "./Header";
-import ProjectsPageLayout from "./ProjectsPageLayout";
 
 function Layout({ children }) {
-  const router = useRouter();
-  const [isProjectsPage, setIsProjectsPage] = useState(false);
-
-  useEffect(() => {
-    if (checkIsProjectsPage(router.route.split("/")[1])) {
-      setIsProjectsPage(true);
-    } else {
-      setIsProjectsPage(false);
-    }
-  }, [router.route]);
-
-  const checkIsProjectsPage = (routeFirstElement) =>
-    routeFirstElement === "projects";
-
   return (
     <section className="bg-bgGrey min-h-screen">
       <div className="w-full max-w-screen-xl mx-auto">
         <Header />
         <div className="relative mx-6">
-          {isProjectsPage ? (
-            <main>
-              <ProjectsPageLayout>{children}</ProjectsPageLayout>
-            </main>
-          ) : (
-            <main>{children}</main>
-          )}
+          <main>{children}</main>
         </div>
       </div>
     </section>
