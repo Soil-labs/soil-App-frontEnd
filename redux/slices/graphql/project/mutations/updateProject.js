6ff80b243d6cef41cfc70188e289cca5bd18bd3d
heavensbee001 <heavensbee001@gmail.com>
2022-07-15 18:45:50 +0200

    Improve UploadProject, not finished yet

--- redux/slices/graphql/project/mutations/updateProject.js
+++ redux/slices/graphql/project/mutations/updateProject.js
@@ -3,14 +3,19 @@ export default function updateProject(params) {
     data: {
       query: `mutation{
 				updateProject(fields:{
-					${params._id?`_id: "${params._id}"`:``}
-					${params.title?`title: "${params.title}"`:``}
-					${params.description?`description: "${params.description}"`:``}
-
-			
-					${params.budget?`budget: ${params.budget}`:``}
-
-					${params.role?`role: ${params.role}`:``}
+					${params._id ? `_id: "${params._id}"` : ``}
+					${params.title ? `title: "${params.title}"` : ``}
+					${params.description ? `description: "${params.description}"` : ``}
+					${params.champion ? `champion: "${params.champion}"` : ``}
+					${params.team ? `team: "${params.team}"` : ``}
+					${params.role ? `role: ${params.role}` : ``}
+					${
+            params.collaborationLinks
+              ? `collaborationLinks: ${params.collaborationLinks}`
+              : ``
+          }
+					${params.budget ? `budget: ${params.budget}` : ``}
+					${params.dates ? `dates: ${params.dates}` : ``}
 	
 			
 				}){
@@ -19,22 +24,61 @@ export default function updateProject(params) {
 					description
 			
 					
-					${params.returnRole?
-					`role{
+					${
+            params.returnRole
+              ? `role{
 						title
 						skills{
 							skill{
 								name
 							}
 						}
-					}`:``}
+					}`
+              : ``
+          }
 
 
-					${params.returnBudget?
-					`budget{
+					${
+            params.returnBudget
+              ? `budget{
 						totalBudget
 						token
-					}`:``}
+					}`
+              : ``
+          }
+
+					  
+					${
+            params.returnTeam
+              ? `team{
+						roleID
+						phase
+						memberInfo {
+						  _id
+						  discordName
+						}`
+              : ``
+          }
+
+
+					${
+            params.returnCollaborationLinks
+              ? `collaborationLinks{
+								title
+								link
+						}`
+              : ``
+          }
+
+
+					${
+            params.returnDates
+              ? `dates{
+								kickOff
+								complition
+						}`
+              : ``
+          }
 			
 				}
 			}`,
--- redux/slices/projectSlice.js
+++ redux/slices/projectSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import apiClient from "../../pages/api/axios";
 import updateProject from "./graphql/project/mutations/updateProject";
 import findProject from "./graphql/project/queries/findProject";
-import {jsonToString} from "../tools/transformations";
+import { jsonToString } from "../tools/transformations";
 
 const initialState = {
   isDataAvailable: false,
@@ -16,26 +16,39 @@ const initialState = {
   role: [],
 };
 
+export const findProject_red = createAsyncThunk(
+  "findProject_red",
+  async (params) => {
+    const response = await apiClient(findProject(params));
 
-
-export const findProject_red = createAsyncThunk("findProject_red",async (params) => {
-
-  const response = await apiClient(findProject(params));
-
-  return response.data.data.findProject;
-}
+    return response.data.data.findProject;
+  }
 );
 
-export const updateProject_red = createAsyncThunk("updateProject_red",async (params) => {
-
-  
-    if (params.budget){
+export const updateProject_red = createAsyncThunk(
+  "updateProject_red",
+  async (params) => {
+    if (params.budget) {
       params.budget = jsonToString(params.budget);
     }
-    if (params.role){
+    if (params.role) {
       params.role = jsonToString(params.role);
     }
-    
+    if (params.champion) {
+      params.champion = jsonToString(params.champion);
+    }
+    if (params.team) {
+      params.team = jsonToString(params.team);
+    }
+    if (params.budget) {
+      params.budget = jsonToString(params.budget);
+    }
+    if (params.dates) {
+      params.dates = jsonToString(params.dates);
+    }
+    if (params.collaborationLinks) {
+      params.collaborationLinks = arrayToString(params.team);
+    }
 
     const response = await apiClient(updateProject(params));
 
@@ -52,20 +65,19 @@ export const projectSlice = createSlice({
       state.loading = true;
     },
     [updateProject_red.fulfilled]: (state, { payload }) => {
-      state.isDataAvailable = true
+      state.isDataAvailable = true;
       state.loading = false;
       state._id = payload._id;
       state.title = payload.title;
       state.description = payload.description;
       state.role = payload.role;
       state.budget = payload.budget;
-
     },
     [findProject_red.pending]: (state) => {
       state.loading = true;
     },
     [findProject_red.fulfilled]: (state, { payload }) => {
-      state.isDataAvailable = true
+      state.isDataAvailable = true;
       state.loading = false;
 
       state._id = payload._id;
@@ -73,7 +85,6 @@ export const projectSlice = createSlice({
       state.description = payload.description;
       state.role = payload.role;
       state.budget = payload.budget;
-
     },
   },
 });
