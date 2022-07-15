3c5908f91bac1f22d3a4b5bb253871d06e303d02
heavensbee001 <heavensbee001@gmail.com>
2022-07-15 16:58:02 +0200

    Add tweets to addSkillToMember

--- redux/slices/graphql/userInspect/mutations/addSkillToMember.js
+++ redux/slices/graphql/userInspect/mutations/addSkillToMember.js
@@ -2,37 +2,37 @@ export default function addSkillToMemberMutation(params) {
   return {
     data: {
       query: `mutation{
-            addSkillToMember(fields:{
-                ${params.skillID ? `skillID: "${params.skillID}"` : ``}
-                ${params.memberID ? `memberID: "${params.memberID}"` : ``}
-                ${params.authorID ? `authorID: "${params.authorID}"` : ``}
-            }){
-                discordName
-                skills {
-                    _id
-                    name
-                    tweets
-                    ${
-                      params.returnMembers
-                        ? `members {
-                        _id
-                        discordName
-                    }`
-                        : ``
-                    }
-                    ${
-                      params.returnAuthors
-                        ? `authors {
-                        _id
-                        discordName
-                    }`
-                        : ``
-                    }
-                    registeredAt
-                    state
-                }
+				addSkillToMember(fields:{
+					${params.skillID ? `skillID: "${params.skillID}"` : ``}
+					${params.memberID ? `memberID: "${params.memberID}"` : ``}
+					${params.authorID ? `authorID: "${params.authorID}"` : ``}
+				}){
+					discordName
+					skills {
+						_id
+						name
+						${params.returnTweets ? `tweets` : ``}
+						${
+              params.returnMembers
+                ? `members {
+								_id
+								discordName
+						}`
+                : ``
             }
-        }`,
+						${
+              params.returnAuthors
+                ? `authors {
+								_id
+								discordName
+						}`
+                : ``
+            }
+						registeredAt
+						state
+					}
+				}
+			}`,
     },
   };
 }
