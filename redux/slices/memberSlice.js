9e24f0a75148f8aa18fc301ec6c26ac80ede171b
heavensbee001 <heavensbee001@gmail.com>
2022-07-16 10:39:50 +0200

    Add addNewMember mutation to member Slice

--- redux/slices/memberSlice.js
+++ redux/slices/memberSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import apiClient from "../../pages/api/axios";
+import addNewMemberMutation from "./graphql/member/mutations/addNewMember";
 import findMember from "./graphql/member/queries/findMember";
 
 const initialState = {
@@ -14,11 +15,22 @@ const initialState = {
   network: [],
 };
 
-export const findMember_red = createAsyncThunk("findMember_red", async (params) => {
-  const response = await apiClient(findMember(params));
-  
-  return response.data.data.findMember;
-});
+export const addNewMember_red = createAsyncThunk(
+  "addNewMember_red",
+  async (params) => {
+    const response = await apiClient(addNewMemberMutation(params));
+
+    return response.data.data.addNewMember;
+  }
+);
+export const findMember_red = createAsyncThunk(
+  "findMember_red",
+  async (params) => {
+    const response = await apiClient(findMember(params));
+
+    return response.data.data.findMember;
+  }
+);
 
 export const memberSlice = createSlice({
   name: "member",
@@ -26,11 +38,26 @@ export const memberSlice = createSlice({
   reducers: {},
   extraReducers: {
     [findMember_red.pending]: (state) => {
-        state.loading = true;
+      state.loading = true;
     },
     [findMember_red.fulfilled]: (state, { payload }) => {
       state.loading = false;
-      state.isDataAvailable = true
+      state.isDataAvailable = true;
+
+      state._id = payload._id;
+      state.discordName = payload.discordName;
+      state.discordID = payload._id;
+      state.bio = payload.bio;
+      state.skills = payload.skills;
+      state.projects = payload.projects;
+      state.network = payload.network;
+    },
+    [addNewMember_red.pending]: (state) => {
+      state.loading = true;
+    },
+    [addNewMember_red.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.isDataAvailable = true;
 
       state._id = payload._id;
       state.discordName = payload.discordName;
