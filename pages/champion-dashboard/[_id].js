53e9915f91e8f2a00d4202ea6a46c4859ab1863b
subject026 <subject026@protonmail.com>
2022-07-23 14:17:25 +0100

    setup tab nav

--- pages/champion-dashboard/[_id].js
+++ pages/champion-dashboard/[_id].js
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
+import React from "react";
 import { useSelector } from "react-redux";
 import { ChampionDashboardLayout } from "../../components/layout/ChampiondashboardLayout";
 import { TeamMemberCard } from "../../components/TeamMemberCard";
+import { classNames } from "../../util";
 
 const tabs = [
   { name: "Overview", href: "#", current: true },
@@ -9,7 +11,11 @@ const tabs = [
   { name: "# Committed", href: "#", current: false },
 ];
 
+const ENGAGED_TAB = "ENGAGED";
+const COMMITTED_TAB = "COMMITTED";
+
 const Project = () => {
+  const [currentTab, setCurrentTab] = React.useState(ENGAGED_TAB);
   const router = useRouter();
   const { _id } = router.query;
 
@@ -22,15 +28,39 @@ const Project = () => {
   return (
     <>
       <main className="col-span-3">
-        <section className="rounded-2xl bg-[#8dc2204c]">
-          <div>tab navigation</div>
-          <div className="p-8">
-            <div className="pb-8">
-              Talent that engaged with your shortlist invitation shows up here.
+        {!isDataAvailable && "Fetching data..."}
+        {isDataAvailable && project && (
+          <section className="rounded-2xl overflow-hidden bg-[#8dc2204c]">
+            <div className="grid grid-cols-2">
+              <button
+                className={classNames(
+                  "px-6 py-3",
+                  currentTab === ENGAGED_TAB
+                    ? "bg-transparant"
+                    : "bg-[#8dc22066]"
+                )}
+                onClick={() => setCurrentTab(ENGAGED_TAB)}
+              >
+                ENGAGED TALENT
+              </button>
+              <button
+                className={classNames(
+                  "px-6 py-3",
+                  currentTab === COMMITTED_TAB
+                    ? "bg-transparant"
+                    : "bg-[#8dc22066]"
+                )}
+                onClick={() => setCurrentTab(COMMITTED_TAB)}
+              >
+                COMMITTED TEAM
+              </button>
             </div>
-            <div className="px-4 sm:px-0">
-              {!isDataAvailable && "Fetching data..."}
-              {isDataAvailable && project && (
+            <div className="p-8">
+              <div className="pt-6 pb-8">
+                Talent that engaged with your shortlist invitation shows up
+                here.
+              </div>
+              <div className="px-4 sm:px-0">
                 <div className="flex flex-col gap-8">
                   {project.info.team.map((member, i) => (
                     <TeamMemberCard
@@ -39,10 +69,10 @@ const Project = () => {
                     />
                   ))}
                 </div>
-              )}
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        )}
       </main>
     </>
   );
