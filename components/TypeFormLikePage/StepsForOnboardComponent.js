6e6d1974f40e6125403716f3b70ca6881404c2e3
sbelka-1703 <sbelka93@gmail.com>
2022-07-29 13:12:22 -0700

    cleaned up code

--- components/TypeFormLikePage/StepsForOnboardComponent.js
+++ components/TypeFormLikePage/StepsForOnboardComponent.js
@@ -54,90 +54,30 @@ function StepsForOnboardComponent(props) {
             <div className="w-[590px] h-[70px] bg-white mt-20 text-4xl shadow-md rounded-2xl flex items-center justify-center">
               <p className="">STEPS DURING THE ONBOARDING</p>
             </div>
-            <div className=" w-[590px] h-[437px] bg-white rounded-2xl mt-10 text-xs">
-              <div className="flex">
+            <div className="   grid grid-col-3 w-[590px] h-[437px] bg-white rounded-2xl mt-10 text-xs">
+              {/* <div className="flex"> */}
+              <div>
                 <div>
+                  {/* Step Titles */}
                   {stepTitles.map((step, key) => {
                     return (
                       <div key={key}>
                         <div>
-                          {/* <div
-                            style={{
-                              backgroundColor: `${stepTitles[0].color}`,
-                            }}
-                          > */}
                           <div
                             onClick={(event) =>
                               handleClickStepTitles(event, key)
                             }
-                            className={classNames(
-                              false ? "bg-[#E2DDFF]" : "bg-[#234F23]",
-                              "px-4 py-2 mt-4 ml-16 shadow-xl w-fit rounded-2xl text-[10px]"
-                            )}
-                            // className="px-4 py-2 mt-4 ml-16 shadow-xl bg-[#E2DDFF] w-fit rounded-2xl text-[10px]"
-                            // className="px-4 py-2 mt-4 ml-16 shadow-xl bg-[#E2DDFF] w-fit rounded-2xl text-[10px]"
+                            className="px-4 py-2 mt-4 ml-16 shadow-xl w-fit rounded-2xl text-[10px]"
                           >
                             <p>{stepTitles[key].title}</p>
                           </div>
-                          {/* </div> */}
-
-                          {/* <div className="px-4 py-2 mt-4 ml-16 shadow-xl bg-[#E2DDFF] w-fit rounded-2xl text-[10px]">
-                            <p> CLOSE APPLICATION</p>
-                          </div>
-                          <div className="px-4 py-2 mt-6 ml-4 shadow-xl w-fit bg-[#CAFFBD] rounded-2xl">
-                            <p>DISPATCH TOKEN</p>
-                          </div>
-                          <div className="px-4 py-2 mt-8 ml-10 shadow-xl w-fit bg-[#D2EFFF] rounded-2xl text-[7px]">
-                            <p>INTRODUCE TO THE TEAM </p>
-                          </div>
-
-                          <div className="px-4 py-2 mt-6 ml-12 shadow-xl w-fit bg-[#FFEEC1] rounded-2xl text-[7px]">
-                            <p>SCHEDULE A KICK OFF MEETING</p>
-                          </div> */}
                         </div>
-                        {/* <div className="px-4 py-2 mt-8 ml-10 shadow-xl bg-[#FED1E9] w-fit rounded-2xl">
-                          <p>JOIN CHANNEL</p>
-                        </div>
-                        <div className="px-4 py-2 mt-4 ml-16 shadow-xl bg-[#D7FFE7] w-fit rounded-2xl">
-                          <p>MINT NFT</p>
-                        </div>
-                        <div className="px-4 py-2 mt-6 ml-4 shadow-xl  bg-[#FFD8B5] w-fit rounded-2xl">
-                          <p>WARM WELCOME</p>
-                        </div>
-                        <div className="px-4 py-2 mt-8 ml-10 shadow-xl bg-[#FED1E9] w-fit rounded-2xl">
-                          <p>CREATE DEWORK </p>
-                        </div>
-                        <div className="px-4 py-2 mt-4 ml-4 shadow-xl bg-[#C8FCFF] w-fit rounded-2xl">
-                          <p>BUILD A SMART CONTRACT</p>
-                        </div>
-                        <div className="px-4 py-2 mt-6 ml-12 shadow-xl w-fit bg-[#DAD7FF] rounded-2xl">
-                          <p>SIGN TRANSACTION</p>
-                        </div> */}
                       </div>
                     );
                   })}
-
-                  {/* <div className="px-4 py-2 mt-8 ml-10 shadow-xl bg-[#FED1E9] w-fit rounded-2xl">
-                    <p>JOIN CHANNEL</p>
-                  </div>
-                  <div className="px-4 py-2 mt-4 ml-16 shadow-xl bg-[#D7FFE7] w-fit rounded-2xl">
-                    <p>MINT NFT</p>
-                  </div>
-                  <div className="px-4 py-2 mt-6 ml-4 shadow-xl  bg-[#FFD8B5] w-fit rounded-2xl">
-                    <p>WARM WELCOME</p>
-                  </div>
-                  <div className="px-4 py-2 mt-8 ml-10 shadow-xl bg-[#FED1E9] w-fit rounded-2xl">
-                    <p>CREATE DEWORK </p>
-                  </div>
-                  <div className="px-4 py-2 mt-4 ml-4 shadow-xl bg-[#C8FCFF] w-fit rounded-2xl">
-                    <p>BUILD A SMART CONTRACT</p>
-                  </div>
-                  <div className="px-4 py-2 mt-6 ml-12 shadow-xl w-fit bg-[#DAD7FF] rounded-2xl">
-                    <p>SIGN TRANSACTION</p>
-                  </div> */}
                 </div>
                 <div className="mt-16">
-                  {" "}
+                  {/* Steps */}
                   {steps.map((step, key) => {
                     return (
                       <div
@@ -156,24 +96,6 @@ function StepsForOnboardComponent(props) {
                     );
                   })}
                 </div>
-                {/* <div>
-                  <div className="px-4 py-2 mt-8 ml-10 shadow-xl bg-[#F8FDC1] w-fit rounded-2xl">
-                    <p>MESSAGE CANDIDATE</p>
-                  </div>
-                  <div className="px-4 py-2 mt-4 ml-16 shadow-xl bg-[#E2DDFF] w-fit rounded-2xl text-[10px]">
-                    <p> CLOSE APPLICATION</p>
-                  </div>
-                  <div className="px-4 py-2 mt-6 ml-4 shadow-xl w-fit bg-[#CAFFBD] rounded-2xl">
-                    <p>DISPATCH TOKEN</p>
-                  </div>
-                  <div className="px-4 py-2 mt-8 ml-10 shadow-xl w-fit bg-[#D2EFFF] rounded-2xl text-[7px]">
-                    <p>INTRODUCE TO THE TEAM </p>
-                  </div>
-
-                  <div className="px-4 py-2 mt-6 ml-12 shadow-xl w-fit bg-[#FFEEC1] rounded-2xl text-[7px]">
-                    <p>SCHEDULE A KICK OFF MEETING</p>
-                  </div>
-                </div> */}
               </div>
             </div>
             <button
