cc49d0b753438e5f86487b7382b366d73730fd6b
heavensbee001 <heavensbee001@gmail.com>
2022-07-29 11:38:31 +0200

    Add user edit workflow working

---
+++ components/EditUser.js
@@ -0,0 +1,100 @@
+import { Fragment, useState, useCallback, useEffect } from "react";
+import SkillSelector from "./skill/SkillSelector";
+import Selector from "./Selector";
+import Avatar from "./Avatar";
+import Textarea from "./TextArea";
+
+const timezones = [
+  "UTC-4",
+  "UTC-3",
+  "UTC-2",
+  "UTC-1",
+  "UTC",
+  "UTC+1",
+  "UTC+2",
+  "UTC+3",
+  "UTC+4",
+];
+const hoursAvailable = [
+  "5 hrs",
+  "10 hrs",
+  "15 hrs",
+  "20 hrs",
+  "25 hrs",
+  "30 hrs",
+  "35 hrs",
+  "40 hrs",
+];
+const periods = ["week", "month"];
+
+export default function EditUser({ user = {}, setUserCallback }) {
+  const [skills, setSkills] = useState([]);
+
+  const setUserInfoCallback = useCallback(
+    (item) => {
+      console.log(item);
+      if (setUserCallback) setUserCallback({ ...user, ...item });
+    },
+    [setUserCallback, user]
+  );
+
+  useEffect(() => {
+    setUserInfoCallback({ skills });
+  }, [skills]);
+
+  return (
+    <>
+      <Avatar src={user?.discordAvatar} />
+      <div>{user?.discordName}</div>
+      <section className="grid grid-cols-2">
+        <div className="col-span-1">
+          <SkillSelector setSkillsCallback={setSkills} showSelected={true} />
+        </div>
+        <div className="col-span-1">
+          <div className="w-full text-center">
+            <div className="w-1/2 inline-block">
+              <Selector
+                setDataCallback={setUserInfoCallback}
+                name="timezone"
+                options={timezones}
+                placeholder="timezone"
+              />
+            </div>
+          </div>
+          <div className="w-1/2 inline-block pr-1">
+            <Selector
+              setDataCallback={setUserInfoCallback}
+              name="hours"
+              options={hoursAvailable}
+              placeholder="hrs"
+            />
+          </div>
+          <div className="w-1/2 inline-block pl-1">
+            <Selector
+              setDataCallback={setUserInfoCallback}
+              name="period"
+              options={periods}
+              placeholder="week/month"
+            />
+          </div>
+          <div className="w-full">
+            <Textarea
+              name="interests"
+              setDataCallback={setUserInfoCallback}
+              placeholder="Interests"
+              title="Interests:"
+            />
+          </div>
+        </div>
+      </section>
+      <div className="w-1/2 mx-auto">
+        <Textarea
+          name="message"
+          setDataCallback={setUserInfoCallback}
+          placeholder="Message"
+          title="Message:"
+        />
+      </div>
+    </>
+  );
+}
--- pages/onboard/index.js
+++ pages/onboard/index.js
@@ -1,25 +1,94 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Layout from "../../components/layout/Layout";
-import SkillSelector from "../../components/skill/SkillSelector";
+import Avatar from "../../components/Avatar";
+import EditUser from "../../components/EditUser";
+
+const mockData = {
+  users: [
+    {
+      discordName: "Milo",
+      discordAvatar: "https://placeimg.com/480/480/nature",
+    },
+    {
+      discordName: "Sbelka",
+      discordAvatar: "https://placeimg.com/480/480/nature",
+    },
+  ],
+};
 
 function Projects() {
-  const [skills, setSkills] = useState([]);
+  const [users, setUsers] = useState(mockData.users);
+  const [currentUserIndex, setCurrentUserIndex] = useState(null);
+  const [savedUsers, setSavedUsers] = useState([]);
+
+  const handleEditUser = (userData) => {
+    const updatedUsers = users.map((user, index) => {
+      if (currentUserIndex !== index) return user;
+      return userData;
+    });
+    setUsers(updatedUsers);
+  };
+
+  const handleSaveUser = () => {
+    setSavedUsers([...savedUsers, users[currentUserIndex]]);
+    setUsers(users.filter((user, index) => index !== currentUserIndex));
+    setCurrentUserIndex(null);
+  };
+
+  const handleDeleteUser = () => {
+    setUsers(users.filter((user, index) => index !== currentUserIndex));
+    setCurrentUserIndex(null);
+  };
+
   return (
     <div className="grid grid-cols-1 gap-y-3 md:gap-x-3 md:grid-cols-5">
       {/* How to apply column */}
-      <section className="col-span-1"></section>
+      <section className="col-span-1">
+        {users.map((user, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              console.log(index);
+              setCurrentUserIndex(index);
+            }}
+          >
+            <Avatar src={user.discordAvatar} />
+            <span>{user.discordName}</span>
+          </div>
+        ))}
+      </section>
 
       {/* Main column */}
       <main className="col-span-3">
-        {/* demo parent data from SkillSelector */}
-        {/* {skills.map((skill, index) => (
-          <span key={index}>{skill.name}</span>
-        ))} */}
-        <SkillSelector setSkillsCallback={setSkills} showSelected={true} />
+        {currentUserIndex !== null && (
+          <>
+            <h3>FILL OUT NEW USER PROFILE</h3>
+            <EditUser
+              user={users[currentUserIndex]}
+              setUserCallback={handleEditUser}
+            />
+            <button onClick={handleSaveUser}>Save</button>
+            <button onClick={handleDeleteUser}>Delete</button>
+          </>
+        )}
       </main>
 
       {/* How to apply column */}
-      <section className="col-span-1"></section>
+      <section className="col-span-1">
+        {savedUsers.map((user, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              console.log(index);
+              setCurrentUserIndex(index);
+            }}
+          >
+            <Avatar src={user.discordAvatar} />
+            <span>{user.discordName}</span>
+          </div>
+        ))}
+      </section>
+      <p>{JSON.stringify(users[currentUserIndex])}</p>
     </div>
   );
 }
