b84bb7a78ba3e89734e1b9548dd07173690be249
heavensbee001 <heavensbee001@gmail.com>
2022-08-03 17:40:41 +0200

    Replace graphql uri with env variable

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -5,21 +5,21 @@ import axios from "axios";
 //   ? process.env.REACT_APP_DOMAIN_NAME + "/graphql"
 //   : "http://localhost:5001/graphql";
 
-const baseURL = "https://oasis-bot-test-deploy.herokuapp.com/graphql";
+const baseURL = process.env.NEXT_PUBLIC_GRAPHQL_URI;
 //  const baseURL = "https://rickandmortyapi.com/graphql"
 
 const apiClient = axios.create({
-    baseURL,
-    method: "POST",
+  baseURL,
+  method: "POST",
 });
 
 apiClient.interceptors.request.use(function (config) {
-    // const token = localStorage.getItem("token");
+  // const token = localStorage.getItem("token");
 
-    // config.headers["Authorization"] = "Bearer " + token;
-    config.headers["Access-Control-Allow-Origin"] = "*";
+  // config.headers["Authorization"] = "Bearer " + token;
+  config.headers["Access-Control-Allow-Origin"] = "*";
 
-    return config;
+  return config;
 }, null);
 
 export default apiClient;
