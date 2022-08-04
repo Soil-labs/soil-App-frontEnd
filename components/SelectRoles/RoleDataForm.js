6e6080ffc4a7438a21da720823fe6cc1f3cd9867
heavensbee001 <heavensbee001@gmail.com>
2022-08-04 16:19:27 +0200

    Add description to form

--- components/SelectRoles/RoleDataForm.js
+++ components/SelectRoles/RoleDataForm.js
@@ -14,12 +14,27 @@ export default function RoleDataForm({
     <div>
       <h3>here goes the form</h3>
       <p>{role.title}</p>
-      <SkillSelector
-        key={role._id}
-        setSkillsCallback={setSkillsCallback}
-        value={role.skills}
-        showSelected={true}
-      />
+      <section className="grid grid-cols-2">
+        <div className="col-span-1 pr-2">
+          <textarea
+            defaultValue={role.description}
+            rows={4}
+            className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
+            onChange={(e) =>
+              setRoleCallback({ ...role, description: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="col-span-1 pl-2">
+          <SkillSelector
+            key={role._id}
+            setSkillsCallback={setSkillsCallback}
+            value={role.skills}
+            showSelected={true}
+          />
+        </div>
+      </section>
       <button
         onClick={() => {
           saveRoleCallback(role);
