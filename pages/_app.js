a64fdfbe6f0487f3566500d62df33cf4faa71f7b
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2022-08-07 00:55:42 +0530

    fix: vercel error

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-
-function MyApp({ Component, pageProps }) {
+import { SessionProvider } from "next-auth/react";
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        {getLayout(<Component {...pageProps} />)}
+      </Provider>
+    </SessionProvider>
   );
 }
 
