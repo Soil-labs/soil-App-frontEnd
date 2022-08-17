c940d4c94d0c084a88531ccdc9620b3f991bf373
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-08-17 17:30:19 +0200

    Initial commit

--- pages/form/index.js
+++ pages/form/index.js
@@ -16,7 +16,7 @@ import TitleComponent from "../../components/TypeFormLikePage/TitleComponent";
 import BudgetComponentNew from "../../components/TypeFormLikePage/BudgetComponentNew";
 
 function Form() {
-  const [phase, setPhase] = useState(0);  
+  const [phase, setPhase] = useState(0);
 
   const changePhase = (phaseNow) => {
     if (phaseNow <= 6) {
@@ -25,11 +25,11 @@ function Form() {
   };
 
   const changePhaseBack = (phaseNow) => {
-    console.log("phaseNow", phaseNow)
+    console.log("phaseNow", phaseNow);
     if (phaseNow >= 0) {
       setPhase((phaseNow -= 1));
     }
-  }
+  };
 
   const skills = useSelector((state) => state.skillsInspect.skillsInfo);
   const _id = useSelector((state) => state.projectInspect._id);
@@ -43,61 +43,71 @@ function Form() {
   const stepsJoinProject = useSelector((state) => state.projectInspect.steps);
 
   return (
+    // <>
+    //   {/* <TitleComponent changePhase={changePhase} phase={phase} /> */}
+    //   {phase == 0 ? (
+    //     // <FormComponent
+    //     //   fieldTitle="What’s the tilte of the new project?"
+    //     //   changePhase={changePhase}
+    //     //   phase={phase}
+    //     //   // submitReply={submitReply}
+    //     // />
+    //     <TitleComponent changePhase={changePhase} phase={phase} />
+    //   ) : phase == 1 ? (
+    //     <DescriptionComponent
+    //       fieldTitle="Description of the new project?"
+    //       changePhase={changePhase}
+    //       changePhaseBack={changePhaseBack}
+    //       phase={phase}
+    //       _id={_id}
+    //     />
+    //   ) : phase == 2 ? (
+    //     <BudgetComponentNew changePhase={changePhase} phase={phase} _id={_id} />
+    //   ) : phase == 3 ? (
+    //     <ProjectSelectRoles changePhase={changePhase} phase={phase} _id={_id} />
+    //   ) : phase == 4 ? (
+    //     <ColabEnvComponent changePhase={changePhase} phase={phase} _id={_id} />
+    //   ) : phase == 5 ? (
+    //     <StepsForOnboardComponent
+    //       changePhase={changePhase}
+    //       phase={phase}
+    //       _id={_id}
+    //     />
+    //   ) : phase == 6 ? (
+    //     <ProjectBoard
+    //       changePhase={changePhase}
+    //       phase={phase}
+    //       projectTitle={title}
+    //       description={description}
+    //       budget={budget}
+    //       links={links}
+    //       dates={dates}
+    //       stepsJoinProject={stepsJoinProject}
+    //       _id={_id}
+    //     />
+    //   ) : phase == 7 ? (
+    //     <YouDidItComponet />
+    //   ) : (
+    //     phase
+    //   )}
+
+    //   {/* <ScopeRolesComponent/> */}
+    //   {/* <GeneralGreenFromComponent/> */}
+    //   {/* <RoleComponent
+    //   skills = {skills}
+    //    /> */}
+    //   {/* <ColabEnvComponent/> */}
+    //   {/* <StepsForOnboardComponent/> */}
+    // </>
+
     <>
-      {/* <TitleComponent changePhase={changePhase} phase={phase} /> */}
       {phase == 0 ? (
-        // <FormComponent
-        //   fieldTitle="What’s the tilte of the new project?"
-        //   changePhase={changePhase}
-        //   phase={phase}
-        //   // submitReply={submitReply}
-        // />
         <TitleComponent changePhase={changePhase} phase={phase} />
       ) : phase == 1 ? (
-        <DescriptionComponent
-          fieldTitle="Description of the new project?"
-          changePhase={changePhase}
-          changePhaseBack={changePhaseBack}
-          phase={phase}
-          _id={_id}
-        />
-      ) : phase == 2 ? (
-        <BudgetComponentNew changePhase={changePhase} phase={phase} _id={_id} />
-      ) : phase == 3 ? (
         <ProjectSelectRoles changePhase={changePhase} phase={phase} _id={_id} />
-      ) : phase == 4 ? (
-        <ColabEnvComponent changePhase={changePhase} phase={phase} _id={_id} />
-      ) : phase == 5 ? (
-        <StepsForOnboardComponent
-          changePhase={changePhase}
-          phase={phase}
-          _id={_id}
-        />
-      ) : phase == 6 ? (
-        <ProjectBoard
-          changePhase={changePhase}
-          phase={phase}
-          projectTitle={title}
-          description={description}
-          budget={budget}
-          links={links}
-          dates={dates}
-          stepsJoinProject={stepsJoinProject}
-          _id={_id}
-        />
-      ) : phase == 7 ? (
-        <YouDidItComponet />
       ) : (
         phase
       )}
-      
-      {/* <ScopeRolesComponent/> */}
-      {/* <GeneralGreenFromComponent/> */}
-      {/* <RoleComponent
-      skills = {skills}
-       /> */}
-      {/* <ColabEnvComponent/> */}
-      {/* <StepsForOnboardComponent/> */}
     </>
   );
 }
